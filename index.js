d4ab9b002ebd858428a6d821a163b91194a35654
OpeDada <opedada@gmail.com>
2024-01-14 22:57:05 +0900

    implemented read

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ function onFormSubmit(){
 
 }
 
+//READ
 function readFormData(){
   let formData = {}
   formData["productCode"] = document.getElementById("productCode").value
+  formData["product"] = document.getElementById("product").value
+  formData["qty"] = document.getElementById("qty").value
+  formData["price"] = document.getElementById("price").value
 }
+
+//CREATE
