c85e15a59517158fdb629677acc947e5a756f1c1
OpeDada <opedada@gmail.com>
2024-01-28 00:54:45 +0900

    implemented create

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let selectedRow = null;
-function onFormSubmit(){
-
+function onFormSubmit(e){
+  e.preventdefault()
 }
 
 //READ
@@ -13,3 +13,17 @@ function readFormData(){
 }
 
 //CREATE
+function insertNewRecord(data){
+  let table = document.getElementById("storeList").getElementsByTagName('tbody')[0];
+  let newRow = table.insertRow(table.length);
+  let cell1 = newRow.insertCell(0)
+      cell1.innerHTML = data.productCode
+  let cell2 = newRow.insertCell(1)
+      cell2.innerHTML = data.product
+  let cell3 = newRow.insertCell(2)
+      cell3.innerHTML = data.qty
+  let cell4 = newRow.insertCell(3)
+      cell4.innerHTML = data.price
+  let cell5 = newRow.insertCell(4)
+      cell5.innerHTML = `<button>Edit</button> <button>Delete</button>`
+}
