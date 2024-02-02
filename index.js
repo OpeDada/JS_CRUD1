b4ebf7dc1bcb71da5811e785165fed4b918b66ef
OpeDada <opedada@gmail.com>
2024-02-02 20:21:31 +0900

    debugging

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let selectedRow = null;
-function onFormSubmit(e) {
-  e.preventdefault();
+function onFormSubmit(event) {
+  event.preventDefault();
   let formData = readFormData();
   selectedRow === null ? insertNewRecord(formData) : updateRecord(formData);
   resetForm();
@@ -12,14 +12,13 @@ function readFormData() {
   formData["productCode"] = document.getElementById("productCode").value;
   formData["product"] = document.getElementById("product").value;
   formData["qty"] = document.getElementById("qty").value;
-  formData["price"] = document.getElementById("price").value;
+  formData["perPrice"] = document.getElementById("perPrice").value;
+  return formData;
 }
 
 //CREATE
 function insertNewRecord(data) {
-  let table = document
-    .getElementById("storeList")
-    .getElementsByTagName("tbody")[0];
+  let table = document.getElementById("storeList").getElementsByTagName("tbody")[0];
   let newRow = table.insertRow(table.length);
   let cell1 = newRow.insertCell(0);
   cell1.innerHTML = data.productCode;
@@ -28,9 +27,10 @@ function insertNewRecord(data) {
   let cell3 = newRow.insertCell(2);
   cell3.innerHTML = data.qty;
   let cell4 = newRow.insertCell(3);
-  cell4.innerHTML = data.price;
+  cell4.innerHTML = data.perPrice;
   let cell5 = newRow.insertCell(4);
-  cell5.innerHTML = `<button onClick='onEdit(this)'>Edit</button> <button>Delete</button>`;
+  cell5.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
+  console.log("data created")
 }
 
 //EDIT
@@ -39,7 +39,7 @@ function onEdit(td) {
   document.getElementById("productCode").value = selectedRow.cells[0].innerHTML;
   document.getElementById("product").value = selectedRow.cells[1].innerHTML;
   document.getElementById("qty").value = selectedRow.cells[2].innerHTML;
-  document.getElementById("price").value = selectedRow.cells[3].innerHTML;
+  document.getElementById("perPrice").value = selectedRow.cells[3].innerHTML;
 }
 
 //UPDATE
@@ -47,12 +47,12 @@ function updateRecord(formData) {
   selectedRow.cells[0].innerHTML = formData.productCode;
   selectedRow.cells[1].innerHTML = formData.product;
   selectedRow.cells[2].innerHTML = formData.qty;
-  selectedRow.cells[3].innerHTML = formData.price;
+  selectedRow.cells[3].innerHTML = formData.perPrice;
 }
 
 //DELETE
 function onDelete(td) {
-  if (confirm(`Do you want to delete this record?`)) {
+  if (confirm("Do you want to delete this record?")) {
     row = td.parentElement.parentElement;
     document.getElementById("storeList").deleteRow(row.rowIndex);
   }
@@ -61,9 +61,9 @@ function onDelete(td) {
 
 //RESET FORM
 function resetForm() {
-  document.getElementById('productCode').value = "";
-  document.getElementById('product').value = "";
-  document.getElementById('qty').value = "";
-  document.getElementById('perPrice').value = "";
-
+  document.getElementById("productCode").value = "";
+  document.getElementById("product").value = "";
+  document.getElementById("qty").value = "";
+  document.getElementById("perPrice").value = "";
+  selectedRow = null;
 }
